4403f27dc2c6bee6c76b57946f8120e5be31a49f
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-07 17:12:42 +0700

    save to txt file + reformatting

--- main.py
+++ main.py
@@ -1,31 +1,69 @@
 user_prompt = "Enter a todo: "
+todo_file = 'todos.txt'
 
-todos = []
+
+def read_todos():
+    existing_file = open(todo_file, 'r')
+    todo_list = existing_file.readlines()
+    existing_file.close()
+    return todo_list
+
+
+def write_todos(todos_list):
+    new_file = open(todo_file, 'w')
+    new_file.writelines(todos_list)
+    new_file.close()
+
+
+def format_string(string):
+    return string + '\n'
+
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit. complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input(user_prompt)
+            todo = format_string(input(user_prompt))
+
+            todos = read_todos()
+
             todos.append(todo)
+
+            write_todos(todos)
         case "show":
+            todos = read_todos()
+
             for index, item in enumerate(todos):
                 index = index + 1
                 formattedStr = f"{index}-{item}"
                 print(formattedStr)
-        case "edit":
-            editItem = input("which item do you want to edit?")
 
-            if int(editItem) <= len(todos):
-                editItem = int(editItem) - 1
-                todos[editItem] = input("update " + todos[editItem] + " to be: ")
+        case "edit":
+            editItem = int(input("which item do you want to edit?"))
+            todos = read_todos()
+
+            if editItem <= len(todos):
+                editItem = editItem - 1
+                todos[editItem] = format_string(input("update " + todos[editItem] + " to be: "))
+                write_todos(todos)
             else:
                 print("item doesnt exit")
 
+        case "complete":
+            todos = read_todos()
+            completedItem = int(input("Which item number did you complete?"))
+
+            if completedItem <= len(todos):
+                todos.pop(completedItem - 1)
+                write_todos(todos)
+            else:
+                print("item doesnt exist")
+
         case "exit":
             break
+
         case _:
             print("That's not a valid command")
 
