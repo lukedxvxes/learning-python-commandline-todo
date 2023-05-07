5bf8b17157b00f4c5b51f9852220a71aca0ff660
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-07 10:10:25 +0700

    adds edit fn

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -13,6 +13,12 @@ while True:
         case "show":
             for item in todos:
                 print(item)
+        case "edit":
+            editItem = input("which item do you want to edit?")
+            editItem = editItem.strip()
+            if editItem in todos:
+                editIndex = todos.index(editItem)
+                todos[editIndex] = input("update " + editItem + " to be: ")
         case "exit":
             break
         case _:
