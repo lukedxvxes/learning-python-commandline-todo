58d90921875caed138bb74acf82c81e80f61e471
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-13 16:19:07 +0700

    refactor to functions

--- main.py
+++ main.py
@@ -19,50 +19,78 @@ def format_string(string):
     return string + '\n'
 
 
+def get_user_action():
+    action = input("Type add [todo], show, edit [todo num], complete [todo num] or exit: ")
+    action = action.strip()
+    return action
+
+
+def add_todo(action):
+    todo = format_string(action[4:])
+    all_todos = read_todos()
+    all_todos.append(todo)
+    write_todos(all_todos)
+
+
+def edit_todo(action):
+    edit_item = int(action[5:])
+    todos = read_todos()
+    edit_item = edit_item - 1
+    todos[edit_item] = format_string(input("update " + todos[edit_item] + " to be: "))
+    write_todos(todos)
+
+
+def complete_todo(action):
+    todos = read_todos()
+    completed_item = action[9:]
+    if completed_item == 'all':
+        todos = []
+    else:
+        completed_item = int(action[9:])
+        todos.pop(completed_item - 1)
+    write_todos(todos)
+
+
+def show_todos():
+    all_todos = read_todos()
+    # example of inline for-loop or list comprehension
+    # for each item in todos, return item.strip
+    formatted_todos = [item.strip('\n') for item in all_todos]
+
+    for index, item in enumerate(formatted_todos):
+        index = index + 1
+        formatted_str = f"{index}-{item}"
+        print(formatted_str)
+
+
 while True:
-    user_action = input("Type add [todo], show, edit [todo num], complete [todo num] or exit: ")
-    user_action = user_action.strip()
+    user_action = get_user_action()
 
     if user_action.startswith("add"):
-        todo = format_string(user_action[4:])
-
-        todos = read_todos()
-
-        todos.append(todo)
-
-        write_todos(todos)
+        add_todo(user_action)
 
     elif user_action.startswith("show"):
-        todos = read_todos()
-        # example of inline for-loop or list comprehension
-        # for each item in todos, return item.strip
-        formatted_todos = [item.strip('\n') for item in todos]
-
-        for index, item in enumerate(formatted_todos):
-            index = index + 1
-            formattedStr = f"{index}-{item}"
-            print(formattedStr)
+        show_todos()
 
     elif user_action.startswith("edit"):
-        editItem = int(user_action[5:])
-        todos = read_todos()
-
-        if editItem <= len(todos):
-            editItem = editItem - 1
-            todos[editItem] = format_string(input("update " + todos[editItem] + " to be: "))
-            write_todos(todos)
-        else:
+        try:
+            edit_todo(user_action)
+        except IndexError:
             print("item doesnt exit")
+            continue
+        except ValueError:
+            print("You must enter the number of the todo")
+            continue
 
     elif user_action.startswith("complete"):
-        todos = read_todos()
-        completedItem = int(user_action[9:])
-
-        if completedItem <= len(todos):
-            todos.pop(completedItem - 1)
-            write_todos(todos)
-        else:
-            print("item doesnt exist")
+        try:
+            complete_todo(user_action)
+        except IndexError:
+            print("Item doesnt exist")
+            continue
+        except ValueError:
+            print("You must enter the number of the todo")
+            continue
 
     elif user_action.startswith("exit"):
         break
