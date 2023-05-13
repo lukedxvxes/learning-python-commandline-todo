cc06d84ded9405a9a4e26bd9d1d5556502b62e06
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-13 16:41:13 +0700

    refactor custom fns to new file and import to main

--- main.py
+++ main.py
@@ -1,67 +1,8 @@
-user_prompt = "Enter a todo: "
-todo_file = 'todos.txt'
-
-
-def read_todos():
-    # Using with-context-manager, removes need to manually close the file
-    with open(todo_file, 'r') as file:
-        todo_list = file.readlines()
-        return todo_list
-
-
-def write_todos(todos_list):
-    # Using with-context-manager, removes need to manually close the file
-    with open(todo_file, 'w') as file:
-        file.writelines(todos_list)
-
-
-def format_string(string):
-    return string + '\n'
-
-
-def get_user_action():
-    action = input("Type add [todo], show, edit [todo num], complete [todo num] or exit: ")
-    action = action.strip()
-    return action
-
-
-def add_todo(action):
-    todo = format_string(action[4:])
-    all_todos = read_todos()
-    all_todos.append(todo)
-    write_todos(all_todos)
-
-
-def edit_todo(action):
-    edit_item = int(action[5:])
-    todos = read_todos()
-    edit_item = edit_item - 1
-    todos[edit_item] = format_string(input("update " + todos[edit_item] + " to be: "))
-    write_todos(todos)
-
-
-def complete_todo(action):
-    todos = read_todos()
-    completed_item = action[9:]
-    if completed_item == 'all':
-        todos = []
-    else:
-        completed_item = int(action[9:])
-        todos.pop(completed_item - 1)
-    write_todos(todos)
-
-
-def show_todos():
-    all_todos = read_todos()
-    # example of inline for-loop or list comprehension
-    # for each item in todos, return item.strip
-    formatted_todos = [item.strip('\n') for item in all_todos]
-
-    for index, item in enumerate(formatted_todos):
-        index = index + 1
-        formatted_str = f"{index}-{item}"
-        print(formatted_str)
-
+from functions import get_user_action, \
+    add_todo, \
+    show_todos, \
+    edit_todo, \
+    complete_todo
 
 while True:
     user_action = get_user_action()
